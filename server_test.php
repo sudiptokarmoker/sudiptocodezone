4600526d61ef8aefdd1e720105dcf37141803678
msystem bangladesh <msystemsbd@gmail.com>
2020-07-12 13:48:27 +0600

    Updated

--- server_test.php
+++ server_test.php
@@ -1,6 +1,7 @@
 <?php 
 try {
     $dbh = new PDO('mysql:host=premium72.web-hosting.com;dbname=solvkzto_db_seisdream_sandbox', 'solvkzto_seisdbu', 'P72B%Lh+7I7k');
+    echo "Solve Array Databse Server Connected";
     foreach($dbh->query('SELECT * from FOO') as $row) {
         print_r($row);
     }
