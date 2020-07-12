c53e9653f72502f3aa3fc724dedb5457cb0d3615
msystem bangladesh <msystemsbd@gmail.com>
2020-07-12 13:47:03 +0600

    Updated

--- server_test.php
+++ server_test.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $dbh = new PDO('mysql:host=https://premium72.web-hosting.com;dbname=solvkzto_db_seisdream_sandbox', 'solvkzto_seisdbu', 'P72B%Lh+7I7k');
+    $dbh = new PDO('mysql:host=premium72.web-hosting.com;dbname=solvkzto_db_seisdream_sandbox', 'solvkzto_seisdbu', 'P72B%Lh+7I7k');
     foreach($dbh->query('SELECT * from FOO') as $row) {
         print_r($row);
     }
