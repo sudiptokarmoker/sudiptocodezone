33b88f1ed0d6359296ecc00da99009dc8ba1f029
msystem bangladesh <msystemsbd@gmail.com>
2020-07-12 14:08:05 +0600

    Updated

--- laravel_project/config/app.php
+++ laravel_project/config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     //'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'https://sudiptocodezone.solvearray.com/laravel_sandbox/'),
+    'url' => env('APP_URL', 'https://sudiptocodezone.solvearray.com/laravel_project/'),
 
     'asset_url' => env('ASSET_URL', null),
 
